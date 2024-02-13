b0279ec8163bfc30afbdde5fd979aec3f4fb6a32
Shreyas Sahoo <shreyas@Shreyas-MacBook-Air.local>
2024-02-13 15:06:26 +0530

    Banner Carousel Added

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Login from './App/Screens/LoginScreen/Login';
 import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
 import * as SecureStore from "expo-secure-store"
+import Home from './App/Screens/HomeScreen/Home';
 
 export default function App() {
 
@@ -26,7 +27,7 @@ export default function App() {
     <ClerkProvider tokenCache={tokenCache} publishableKey='pk_test_YW11c2luZy1vcmlvbGUtNTMuY2xlcmsuYWNjb3VudHMuZGV2JA'>
       <SafeAreaView style={styles.container}>
        <SignedIn>
-        <Login />
+        <Home />
           {/* <Text>You are Signed in</Text> */}
         </SignedIn>
         <SignedOut>
